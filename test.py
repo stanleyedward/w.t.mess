e5f08201af9eb0b552339d21c40a3ddc35d4ed2d
stanleyedward <stanleyedward9715@gmail.com>
2023-09-24 11:34:23 +0530

    add meal fn

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import os
-import sys
+import calendar
+import datetime
 import requests
 import json
 from prettytable import PrettyTable
@@ -26,16 +26,37 @@ with open('data.json', 'w') as f:
 table = PrettyTable(
     
 )
-meal_names=["Breakfast", "Lunch", "Snacks", "Dinner"]
-
+def meal_name() -> str:
+    current_time = datetime.datetime.now().time()
+    breakfast_end_time = datetime.time(hour=9, minute=00)
+    lunch_end_time = datetime.time(hour=13, minute=00)
+    snacks_end_time =  datetime.time(hour=17, minute=30)
+    dinner_end_time = datetime.time(hour=21, minute=00)
+    if(breakfast_end_time>current_time):
+        return "Breakfast"
+    elif(lunch_end_time>current_time):
+        return "Lunch"
+    elif(snacks_end_time>current_time):
+        return "Snacks"
+    else:
+        return "Dinner"
+meal = meal_name()
+# meal_names=["Breakfast", "Lunch", "Snacks", "Dinner"]
+current_date = datetime.datetime.now().date()
+day_name = calendar.day_name[current_date.weekday()].lower()
+current_time = datetime.datetime.now().time()
 
 menu = []
-for meals in meal_names:
-    menu.append(response_json['data'][0]['wednesday'][meals])
+# for meals in meal_names:
+menu.append(response_json['data'][0][day_name][meal])
 
 # print(menu)
-for i in range(4):
-    table.add_column(meal_names[i],menu[i])
-    print(table)
-    table.clear()
-# print(table)
+# for i in range(4):
+
+print(f"\n{current_date} | {day_name} | {current_time}")
+
+table.add_column(meal,menu[0])
+print(table)
+    # table.clear()
+# # print(table)
+
