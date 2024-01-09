26bd3b2ea9d4aa0205ef4cbe00faab681dfbbcef
stanleyedward <stanleyedward9715@gmail.com>
2024-01-09 18:42:24 +0530

    black formatting

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 from prettytable import PrettyTable
 import os
 from pathlib import Path
+
 URL = "https://whatsinmess.vercel.app/api/get_menu"
 response = requests.get(URL)
 response_json = response.json()
@@ -15,37 +16,38 @@ days = {
     "Thursday": 3,
     "Friday": 4,
     "Saturday": 5,
-    "Sunday": 6
+    "Sunday": 6,
 }
 data_path = Path("wt_mess/data/")
 if data_path.is_dir():
     pass
     # print('alre there')
 else:
-    data_path.mkdir(parents=True,
-                    exist_ok=True)
-    with open(data_path/'data.json', 'w') as f:
+    data_path.mkdir(parents=True, exist_ok=True)
+    with open(data_path / "data.json", "w") as f:
         json.dump(response_json, f, indent=4)
 # if data.json.is_file()
 
 #
-table = PrettyTable(
-    
-)
+table = PrettyTable()
+
+
 def meal_name() -> str:
     current_time = datetime.datetime.now().time()
     breakfast_end_time = datetime.time(hour=9, minute=00)
     lunch_end_time = datetime.time(hour=13, minute=00)
-    snacks_end_time =  datetime.time(hour=17, minute=30)
+    snacks_end_time = datetime.time(hour=17, minute=30)
     dinner_end_time = datetime.time(hour=21, minute=00)
-    if(breakfast_end_time>current_time):
+    if breakfast_end_time > current_time:
         return "Breakfast"
-    elif(lunch_end_time>current_time):
+    elif lunch_end_time > current_time:
         return "Lunch"
-    elif(snacks_end_time>current_time):
+    elif snacks_end_time > current_time:
         return "Snacks"
     else:
         return "Dinner"
+
+
 meal = meal_name()
 # meal_names=["Breakfast", "Lunch", "Snacks", "Dinner"]
 current_date = datetime.datetime.now().date()
@@ -54,15 +56,14 @@ current_time = datetime.datetime.now().time()
 
 menu = []
 # for meals in meal_names:
-menu.append(response_json['data'][0][day_name][meal])
+menu.append(response_json["data"][0][day_name][meal])
 
 # print(menu)
 # for i in range(4):
 
 print(f"\n{current_date} | {day_name} | {current_time}")
 
-table.add_column(meal,menu[0])
+table.add_column(meal, menu[0])
 print(table)
-    # table.clear()
+# table.clear()
 # # print(table)
-
